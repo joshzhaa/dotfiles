d6005b132bc674125ec27e3785b731db46069f7b
josh <joshzhaa@gmail.com>
2026-08-26 00:23:07 -0700

    wip: statusline implementation, cland config

--- nix/pkgs/statusline/statusline.cc
+++ nix/pkgs/statusline/statusline.cc
@@ -2,7 +2,18 @@
 
 import std;
 
-auto main() -> int {
-  std::println("hello world");
+constexpr std::string_view BATTERY = "/sys/class/power_supply/BAT1";
+
+auto main(int argc, char **argv) -> int {
+  auto args = std::span{argv, static_cast<std::size_t>(argc)};
+
+  namespace fs = std::filesystem;
+
+  auto file = std::ifstream{BATTERY / fs::path("capacity")};
+  std::string content;
+  file >> content;
+
+  std::println("hello world {}", content);
+
   return 0;
 }
